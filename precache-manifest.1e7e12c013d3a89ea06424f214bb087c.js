c2cba2b822ada73527008df0b618117cd594dc38
AlexSwiss <39837597+AlexSwiss@users.noreply.github.com>
2019-06-28 03:43:35 +0100

    Updates

---
+++ precache-manifest.1e7e12c013d3a89ea06424f214bb087c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2d15970f74596a69ca7",
+    "url": "/my-portfolio/static/js/runtime~main.674e5ed7.js"
+  },
+  {
+    "revision": "9c32dac6fcf5040ec3e6",
+    "url": "/my-portfolio/static/js/main.37f9ad52.chunk.js"
+  },
+  {
+    "revision": "439475a28819dfd33536",
+    "url": "/my-portfolio/static/js/2.baf3c1c8.chunk.js"
+  },
+  {
+    "revision": "9c32dac6fcf5040ec3e6",
+    "url": "/my-portfolio/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "439475a28819dfd33536",
+    "url": "/my-portfolio/static/css/2.82b3dac3.chunk.css"
+  },
+  {
+    "revision": "290d1822c493a42d194069357d39d296",
+    "url": "/my-portfolio/index.html"
+  }
+];
